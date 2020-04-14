c8138546a38c8d3fd94f4cf9e3676df7c2c0f8b9
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-14 09:34:01 -0500

    Create engagement.js

---
+++ engagement.js
@@ -0,0 +1,45 @@
+//engagement of targets
+function engageTarget(target) {
+	//check if in range
+	if (is_in_range(target)) {
+		//if in range, either stand or kite depending on their target
+		if (target.target == character.name) {
+			//they are targeting me
+			//kite if we have larger range, otherwise stand
+			if (target.range < character.range) {
+				kite(target);
+			}
+		} else {
+			//they are in range and not targeting us. Hold position
+			if (target.target == "" && distance(character,target) <= target.range) {
+				if (target.range < character.range) {
+					//getting out of range before engaging
+					log("getting out of range before first hit");
+					kite(target);
+				}
+			}
+		}
+		
+	} else {
+		//they are out of range. Approach them
+		if (!character.moving && !seeking) {
+			log("Moving towards target");
+			smarter_move(
+				target.x,
+				target.y,
+				character,
+				target
+			);
+		}
+	}
+	
+	//check if in range and attack
+	if (is_in_range(target) && can_attack(target) && !is_on_cooldown("attack")) {
+		if (target.target == "" && distance(character,target) <= target.range) {
+			//too close. wait until we get out of range
+			log("getting out of range before attacking");
+		} else {
+			attack(target);
+		}
+	}
+}
