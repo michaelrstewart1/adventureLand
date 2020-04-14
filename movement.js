ff1c997d68277f1522665226c5cf2bb1e6049957
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-14 09:33:10 -0500

    Update movement.js

--- movement.js
+++ movement.js
@@ -45,51 +45,6 @@ function kite(target) {
 	}		
 }
 
-function engageTarget(target) {
-	//check if in range
-	if (is_in_range(target)) {
-		//if in range, either stand or kite depending on their target
-		if (target.target == character.name) {
-			//they are targeting me
-			//kite if we have larger range, otherwise stand
-			if (target.range < character.range) {
-				kite(target);
-			}
-		} else {
-			//they are in range and not targeting us. Hold position
-			if (target.target == "" && distance(character,target) <= target.range) {
-				if (target.range < character.range) {
-					//getting out of range before engaging
-					log("getting out of range before first hit");
-					kite(target);
-				}
-			}
-		}
-		
-	} else {
-		//they are out of range. Approach them
-		if (!character.moving && !seeking) {
-			log("Moving towards target");
-			smarter_move(
-				target.x,
-				target.y,
-				character,
-				target
-			);
-		}
-	}
-	
-	//check if in range and attack
-	if (is_in_range(target) && can_attack(target) && !is_on_cooldown("attack")) {
-		if (target.target == "" && distance(character,target) <= target.range) {
-			//too close. wait until we get out of range
-			log("getting out of range before attacking");
-		} else {
-			attack(target);
-		}
-	}
-}
-
 function getNextKiteCoordinates(character, target, reverse, angle, rangeScale) {
 	if (!character || !target) return;
 	var range = character.range * rangeScale;
@@ -124,5 +79,21 @@ function smarter_move(x,y,character,target) {
 		);
 	} else {
 		return smart_move({x:x,y:y});
+	}
+}
+
+function catchUpTo(target) {
+	if (target) {
+		log("Catching up to "+target.name);
+		smarter_move(
+			target.x,
+			target.y,
+			character,
+			target
+		);
+		return;
+	} else {
+		log("No target to catch up to");
+		return;
 	}
 }
