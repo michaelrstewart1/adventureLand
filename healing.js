949bfb492f83628a124fc13ac2a1385006c1156f
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-14 13:22:48 -0500

    Update healing.js

--- healing.js
+++ healing.js
@@ -19,3 +19,49 @@ function use_regen() {
 	}
 	return;
 }
+
+function healParty(party) {
+	var partyArray = [];
+	var tempMember;
+	var partyMember;
+	var numberNeedingHeal = 0;
+	for (let i in party) {
+		partyMember = get_player(party[i]);
+		if (partyMember) {
+			if (partyMember.hp < partyMember.max_hp && is_in_range(partyMember)) {
+				numberNeedingHeal++;
+			}
+			tempMember = {
+				name: partyMember.name,
+				hp: partyMember.hp,
+				max_hp: partyMember.max_hp,
+				percent_hp: partyMember.hp/partyMember.max_hp,
+				in_range: is_in_range(partyMember)
+			}
+			if (is_in_range(partyMember)) {
+				partyArray.push(tempMember);
+			}
+		}
+	}
+	if (numberNeedingHeal > 1 && !is_on_cooldown("partyheal")) {
+		//if (use_skill("partyheal")) {
+		//	return true;
+		//}
+	}
+	
+	partyArray.sort(compare);
+	for (let p in partyArray) {
+		if (partyArray[p].percent_hp < 0.8 && partyArray[p].in_range) {
+			//heal them
+			if (!is_on_cooldown('attack')) {
+				log("Healing "+partyArray[p].name+" at "+partyArray[p].hp+" ("+partyArray[p].percent_hp+"%)");
+				heal(get_player(partyArray[p].name)).then(function(data){reduce_cooldown("heal",character.ping);});
+				return true;
+			} else {
+				set_message("Heal CD");
+				return true;
+			}
+		}
+	}
+	return false;
+}
