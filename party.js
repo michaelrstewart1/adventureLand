378474ad4c0861c00951b3a5dcc02edf2bbf3727
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-16 11:02:41 -0500

    Update party.js

--- party.js
+++ party.js
@@ -3,6 +3,10 @@
 //TODO: need to make the character list dynamic instead of hard-coding
 var players = ['Beef','Pragmus','CohenPlaces','GoldRanger'];
 
+setInterval(function() {
+	update_character_localstorage();
+}, 100);
+
 function update_character_localstorage() {
 	let data = {
 		'name': character.name,
