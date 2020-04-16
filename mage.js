7ac2ec5b7b284328f8aad9fee41b89426aeb09c4
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-16 00:58:30 -0500

    Update mage.js

--- mage.js
+++ mage.js
@@ -1,9 +1,13 @@
 //mage
 
 var attack_mode=true
+var party = get_party();
+var partyKeys = Object.keys(party);
 
 setTimeout(function () {
-	setInterval(function() {
+	setInterval(function(){
+		party = get_party();
+		partyKeys = Object.keys(party);
 		use_regen();
 		loot();
 		doInvites();
