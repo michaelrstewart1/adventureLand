186c790d5bc05723fe2708762f2789742e141be5
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-16 11:09:52 -0500

    Update gui.js

--- gui.js
+++ gui.js
@@ -27,12 +27,42 @@ init_dpsmeter(5);
 
 register_ccmeterhandler("player", ccmeter_playerhandler);    
 register_dpsmeterhandler("hit", dpsmeterHitHandler);
+parent.render_party = new_render_party;
 
 setInterval(function() {
 	update_xptimer()
 	update_ccmeter();
 	update_dpsmeter();
 }, 100);
+
+setInterval(function(){
+	new_render_party();
+}, 1000);
+
+function new_render_party() {
+    var b = "";
+    for (var a in parent.party) {
+        var c = parent.party[a];
+        b += " <div class='gamebutton' style='padding: 6px 8px 6px 8px; font-size: 24px; line-height: 18px' onclick='party_click(\"" + a + "\")'>";
+        b += parent.sprite(c.skin, {
+            cx: c.cx || [],
+            rip: c.rip
+        });
+        if (c.rip) {
+            b += "<div style='color:gray; margin-top: 1px'>RIP</div>"
+        } else {
+            b += "<div style='margin-top: 1px'>" + a.substr(0, 3).toUpperCase() + "</div>"
+        }
+		b += "<div style='margin-top: 1px'>" + (c.share*100).toFixed(0) + "%</div>"
+        b += "</div>"
+    }
+    parent.$("#newparty").html(b);
+    if (!parent.party_list.length) {
+        parent.$("#newparty").hide()
+    } else {
+        parent.$("#newparty").show()
+    }
+}
 
 function init_dpsmeter() {
 
