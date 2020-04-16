0fc131d43e1827088b915b7b8fe5635861382b3a
michaelrstewart1 <31330323+michaelrstewart1@users.noreply.github.com>
2020-04-16 11:04:04 -0500

    Update priest.js

--- priest.js
+++ priest.js
@@ -14,9 +14,7 @@ setTimeout(function () {
 		partyKeys = Object.keys(party);
 		use_regen();
 		loot();
-		update_character_localstorage();
 		doInvites();
-		update_xptimer();
 
 		if (character.party) {
 			var mainAssist = getMainAssist()
